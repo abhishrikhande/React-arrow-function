cc0701e8fde3dc6570a2b83255162d491434659d
abhishrikhande <abhijeetshrikhande1999@gmail.com>
2021-10-07 18:48:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Arrow from './components/Arrow';
+
+
+function App() {
+  return (
+     <div>
+        <Arrow />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/components/Arrow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Arrow = () => {
+return <h1>This Is Section Component</h1>
+}
+
+export default Arrow;
